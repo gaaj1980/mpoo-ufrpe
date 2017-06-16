8eac99937fce3c10952903a5f5a6995360fa971d
Nicollas Bastos <nicoedu_@hotmail.com>
2017-06-16 11:43:36 -0300

    Insercao de Nicollas

--- mpoo-ufrpe/src/br/ufrpe/bsi/mpoo/Application.java
+++ mpoo-ufrpe/src/br/ufrpe/bsi/mpoo/Application.java
@@ -53,18 +53,15 @@ public class Application {
 	private static List<Aluno> criarAlunos() {
 		List<Aluno> alunos = new ArrayList<Aluno>(40);
 		alunos.add(criarAluno("Nome do Aluno 1", "alunoteste1", 2016, Semestre.PRIMEIRO));
-		//gaaj1980: Inclua a partir da linha abaixo, o cï¿½digo para incluir 
+		//gaaj1980: Inclua a partir da linha abaixo, o código para incluir 
 		//um aluno com os seus dados, conforme exemplo da linha acima 
-		//um aluno com os seus dados, conforme exemplo da linha acima
 		alunos.add(criarAluno("Eric Moritsuka", "Ericthm", 2016, Semestre.PRIMEIRO));
 		alunos.add(criarAluno("Ebony Marques Rodrigues", "EbonyMarques", 2016, Semestre.PRIMEIRO));
 		alunos.add(criarAluno("Joï¿½o Josï¿½ Antonio Souza D'Amorim", "joao-damorim", 2015, Semestre.PRIMEIRO));
 		alunos.add(criarAluno("Eliana Maria silva de FranÃ§a", "elianamsf", 2015, Semestre.SEGUNDO));
 		alunos.add(criarAluno("Edson Cristovam", "Crissky", 2015, Semestre.SEGUNDO));
 		alunos.add(criarAluno("Luiz Carlos Brito de Andrade Lima Filho", "xRuisux", 2016, Semestre.PRIMEIRO));
-		
-
-		
+		alunos.add(criarAluno("Nicollas Bastos", "nicoedu", 2016, Semestre.PRIMEIRO));
 		return alunos;
 	}
 }
